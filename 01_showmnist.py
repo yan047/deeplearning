fc78b7a5ca66568afb12d7aefd4d61609edad4b5
yan047 <bobyan11@gmail.com>
2019-09-06 16:32:36 +1000

    add data type information for mnist dataset

--- 01_showmnist.py
+++ 01_showmnist.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+# x_train is type numpy.ndarray, [28][28][60000] of uint8; 3D.
+# y_train is type numpy.ndarray, [60000] of uint8; 1D.
+
 plt.subplot(221)
 plt.imshow(x_train[0], cmap=plt.get_cmap('gray'))
 plt.subplot(222)
