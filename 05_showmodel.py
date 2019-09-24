d9dbc80ba28c1ff23fac7fa1de74acbea05c8d85
yan047 <bobyan11@gmail.com>
2019-09-24 20:48:27 +1000

    create model diagram

---
+++ 05_showmodel.py
@@ -0,0 +1,17 @@
+from keras import layers, models
+from keras.utils import plot_model
+
+# create deep network.
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+# feed into classifier.
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+plot_model(model, to_file='model.png')
